0a7c8e565907cae578a079393712e0ba865c3a82
leo6liu <leo.battalora@gmail.com>
2022-07-12 00:24:03 -0400

    old and new tiers added

--- assets/tiers.ts
+++ assets/tiers.ts
@@ -1,11 +1,30 @@
+/** Image URLs for tier emblems. */
+export enum Emblems {
+  /** Duplicate of OldEmblems.UNRANKED (Unranked tier was not provided an image asset with new tier emblems). */
+  UNRANKED = "https://i.imgur.com/0NvMghR.png",
+  IRON = "https://i.imgur.com/UDEcSMa.png",
+  BRONZE = "https://i.imgur.com/I8Z1hpu.png",
+  SILVER = "https://i.imgur.com/IDqYSlB.png",
+  GOLD = "https://i.imgur.com/K8lpz9H.png",
+  PLATINUM = "https://i.imgur.com/nC66P0k.png",
+  DIAMOND = "https://i.imgur.com/KuxtFOQ.png",
+  MASTER = "https://i.imgur.com/a2FCvGx.png",
+  GRANDMASTER = "https://i.imgur.com/GbBDAw8.png",
+  CHALLENGER = "https://i.imgur.com/3tV1TBa.png",
+}
+
 /** Image URLs for old tier emblems. */
 export enum OldEmblems {
   UNRANKED = "https://i.imgur.com/0NvMghR.png",
+  /** Duplicate of OldEmblems.IRON (Iron tier did not exist with old tier emblems). */
+  IRON = "https://i.imgur.com/UDEcSMa.png",
   BRONZE = "https://i.imgur.com/d22l4ph.png",
   SILVER = "https://i.imgur.com/bTeiOwm.png",
   GOLD = "https://i.imgur.com/oOF5fur.png",
   PLATINUM = "https://i.imgur.com/Drwx9pI.png",
   DIAMOND = "https://i.imgur.com/YUUbcv3.png",
   MASTER = "https://i.imgur.com/fctHEaU.png",
+  /** Duplicate of OldEmblems.MASTER (Grandmaster tier did not exist with old tier emblems). */
+  GRANDMASTER = "https://i.imgur.com/fctHEaU.png",
   CHALLENGER = "https://i.imgur.com/f1HVL7t.png",
 }
