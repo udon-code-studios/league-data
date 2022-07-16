883e27a2b3a3b6880b15105d3baf36e63ae1650a
leo6liu <leo.battalora@gmail.com>
2022-07-16 17:00:25 -0400

    data module exports

---
+++ data/mod.ts
@@ -0,0 +1,3 @@
+export * as champion from "./champion.ts";
+export * as item from "./item.ts";
+export * as summoner from "./summoner.ts";
--- mod.ts
+++ mod.ts
@@ -5,3 +5,4 @@
  */
 
 export * as assets from "./assets/mod.ts";
+export * as data from "./data/mod.ts";
